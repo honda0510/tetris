44f03679db183b916a41b0c16090466dc4a3ba86
Masayoshi Honda <masayoshi.honda@gmail.com>
2020-03-07 16:19:04 +0900

    Fix getMino()

--- js/tetrimino.js
+++ js/tetrimino.js
@@ -34,7 +34,7 @@ function getMino() {
     //MINOS からランダムで id を取得する
     var id = Math.floor(Math.random() * MINOS.length);
     var mino = [];
-    for (var y = 0; y < 2; y++) {
+    for (var y = 0; y < 4; y++) {
         mino[y] = [];
         for (var x = 0; x < 4; x++) {
             mino[y][x] = 0;
